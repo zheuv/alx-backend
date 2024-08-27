8a5f11720532c64a7012c195892c943754cb7406
mouhcine wildane <muhssenwildane@gmail.com>
2024-08-27 21:11:27 +0100

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  """ Doc for function """
-  render_template("templates/index.html")
+    """ Doc for function """
+    render_template("templates/index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
