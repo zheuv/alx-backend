d4a920f1c4a241d9c19e79bb67f633d736a3002d
mouhcine wildane <muhssenwildane@gmail.com>
2024-08-27 22:48:47 +0000

    test

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Doc for script """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+import os
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', 
+                           home_title=_("home_title"), 
+                           home_header=_("home_header"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
