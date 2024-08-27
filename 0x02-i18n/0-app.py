c4b8f6031a5564a9a9ed74d01764d65ba317a411
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-27 21:12:16 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ Doc for function """
-    render_template("templates/index.html")
+    render_template("index.html")
 
 
 if __name__ == '__main__':
